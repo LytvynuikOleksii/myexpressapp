8032dbaab61f8c8902165a4935c9c86a9c076d89
Oleksii Lytvyniuk <snaykmini609@gmail.com>
2024-09-19 14:16:00 +0300

    Add pagination

--- app.js
+++ app.js
@@ -22,8 +22,13 @@ const userSchema = Joi.object({
 
 // Перегляд всіх користувачів
 app.get("/users", async (req, res) => {
+  const page = parseInt(req.query.page) || 1;
+  const limit = parseInt(req.query.limit) || 10;
+  const startIndex = (page -1) * limit;
+  const endIndex = page * limit;
   try {
     const users = await prisma.user.findMany();
+    const usersSlice = users.slice(startIndex, endIndex);
     res.json(users);
   } catch (err) {
     res.status(400).json({ error: err.message });
