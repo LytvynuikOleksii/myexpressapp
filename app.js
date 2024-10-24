44c152876072ec22dcdf5787ed53c6c48dacf622
Oleksii Lytvyniuk <snaykmini609@gmail.com>
2024-10-24 09:19:18 +0300

    App sassions support

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const { PrismaClient } = require("@prisma/client");
 const Joi = require("joi");
 const NodeCache = require("node-cache");
 const bcrypt = require("bcrypt");
+const session = require("express-session");
 
 const prisma = new PrismaClient();
 const cache = new NodeCache();
@@ -15,6 +16,14 @@ app.use((req, res, next) => {
   console.log("Метод", req.method, "і шлях", req.path, "запиту.");
   next();
 });
+
+app.use(
+  session({
+    secret: "your_secretkey_here",
+    resave: false,
+    saveUninitialized: true,
+  })
+);
 
 const userSchema = Joi.object({
   name: Joi.string().min(3).max(30).required(),
@@ -161,9 +170,21 @@ app.post("/login", async (req, res) => {
       return res.status(401).send("Invalid password");
     }
 
+    req.session.username = user.name;
+    req.session.userId = user.id;
+
     res.status(200).send("Login successful");
   } catch (err) {
     res.status(500).send("Login error");
+    console.log(err);
+  }
+});
+
+app.get("/profile", async (req, res) => {
+  if (req.session.username) {
+    res.send(`Hi, ${req.session.username}`);
+  } else {
+    res.send("Please log in");
   }
 });
 
